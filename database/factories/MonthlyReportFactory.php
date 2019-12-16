d9039ef553377ffae800bc2aa16960f802736de1
yasuaki640 <yasuaki640@gmail.com>
2019-12-16 22:24:39 +0900

    カラムの順番を変更

--- database/factories/MonthlyReportFactory.php
+++ database/factories/MonthlyReportFactory.php
@@ -7,9 +7,10 @@ use Faker\Generator as Faker;
 
 $factory->define(MonthlyReport::class, function (Faker $faker) {
     return [
-        'timeStamp' => now(),
         'numOfKumite' => $faker->numberBetween(0, 10),
         'numOfNonKumite' => $faker->numberBetween(0, 10),
-        'numOfVisitors' => $faker->numberBetween(0, 10)
+        'numOfVisitors' => $faker->numberBetween(0, 10),
+        'created_at' => now(),
+        'updated_at' => now(),
     ];
 });
