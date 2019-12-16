42828ecedeb0b667d0a97bb1ae0dac778bfc2eac
yasuaki640 <yasuaki640@gmail.com>
2019-12-16 21:31:53 +0900

    テストデータ投入用シーだーファイルを作成

---
+++ database/seeds/MonthlyReportTableSeeder.php
@@ -0,0 +1,19 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use \App\MonthlyReport;
+
+class MonthlyReportTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+        factory(MonthlyReport::class, 10)
+            ->create();
+    }
+}
