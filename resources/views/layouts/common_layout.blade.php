836b090b13314a02cab4c54e556c580140eb116a
yasuaki640 <yasuaki640@gmail.com>
2019-12-20 23:12:47 +0900

    共通に読み込むbladeファイルを作成

---
+++ resources/views/layouts/common_layout.blade.php
@@ -0,0 +1,15 @@
+<!
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="chrome">
+    <link href="{{ url("/css/bootstrap/bootstrap.css") }}" rel="stylesheet" type="text/css">
+    <script src="{{url("/resources/js/bootstrap.js")}}"></script>
+    <title>SHUKEI - @yield('title')</title>
+</head>
+<body>
+@yield('content')
+</body>
+</html>
