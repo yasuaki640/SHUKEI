82f834c995ff7dbddf49278e59998604fd96aeae
yasuaki640 <yasuaki640@gmail.com>
2019-12-21 11:36:51 +0900

    アセットのコンパイルを導入

--- resources/views/layouts/common_layout.blade.php
+++ resources/views/layouts/common_layout.blade.php
@@ -5,11 +5,14 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="chrome">
-    <link href="{{ url("/css/bootstrap/bootstrap.css") }}" rel="stylesheet" type="text/css">
-    <script src="{{url("/resources/js/bootstrap.js")}}"></script>
+    <link href="{{ mix("css/app.css")}}" rel="stylesheet" type="text/css">
+
     <title>SHUKEI - @yield('title')</title>
 </head>
+
 <body>
 @yield('content')
 </body>
+
+<script src="{{mix("js/app.js")}}"></script>
 </html>
