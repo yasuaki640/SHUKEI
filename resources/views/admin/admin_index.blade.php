8ff0511bc5d0b0c49f35a8a8dbfdb835d8712fe1
yasuaki640 <yasuaki640@gmail.com>
2019-12-21 12:33:59 +0900

    bootstrapのテーブルクラスを定義

--- resources/views/admin/admin_index.blade.php
+++ resources/views/admin/admin_index.blade.php
@@ -3,5 +3,23 @@
 @section('title','管理者画面')
 
 @section('content')
-    <p class="text-center text-success">hello world</p>
+    <table class="table">
+        <thead>
+        <tr>
+            <th>id</th>
+            <th scope="col">名前</th>
+            <th scope="col">組手人数</th>
+            <th scope="col">未組手人数</th>
+            <th scope="col">未組手来場者数</th>
+        </tr>
+        </thead>
+        <tbody>
+        <tr>
+            <th scope="row">1</th>
+            <td>テーブルのセル</td>
+            <td>テーブルのセル</td>
+            <td>テーブルのセル</td>
+        </tr>
+        </tbody>
+    </table>
 @endsection
