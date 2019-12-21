590bfe2f63af8fc0da6ef8c3e6b30a02d4c4b20b
yasuaki640 <yasuaki640@gmail.com>
2019-12-21 14:59:44 +0900

    画面に月次報告を表示

--- resources/views/admin/admin_index.blade.php
+++ resources/views/admin/admin_index.blade.php
@@ -11,15 +11,23 @@
             <th scope="col">組手人数</th>
             <th scope="col">未組手人数</th>
             <th scope="col">未組手来場者数</th>
+            <th scope="col">登録日時</th>
+            <th scope="col">更新日時</th>
         </tr>
         </thead>
         <tbody>
-        <tr>
-            <th scope="row">1</th>
-            <td>テーブルのセル</td>
-            <td>テーブルのセル</td>
-            <td>テーブルのセル</td>
-        </tr>
+        @foreach($monthly_reports as $monthly_report)
+            <tr>
+                <th scope="row">{{$monthly_report->id}}</th>
+                <th scope="row">{{$monthly_report->name}}</th>
+                <td>{{$monthly_report->numOfKumite}}</td>
+                <td>{{$monthly_report->numOfNonKumite}}</td>
+                <td>{{$monthly_report->numOfVisitors}}</td>
+                <td>{{$monthly_report->created_at}}</td>
+                <td>{{$monthly_report->updated_at}}</td>
+            </tr>
+        @endforeach
         </tbody>
     </table>
+
 @endsection
