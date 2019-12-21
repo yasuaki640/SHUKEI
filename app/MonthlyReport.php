cddbbeb1d39bb8fec946f289d25152526ee7d617
yasuaki640 <yasuaki640@gmail.com>
2019-12-21 14:58:46 +0900

    日付整形

--- app/MonthlyReport.php
+++ app/MonthlyReport.php
@@ -3,8 +3,11 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class MonthlyReport extends Model
 {
+    use SoftDeletes;
+    protected $dates = ['created_at', 'updated_at',];
     protected $table = 'monthly_report';
 }
