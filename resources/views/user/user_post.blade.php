9cbdf43c5338f3ce8d25324906b27d04965bcd2a
yasuaki640 <yasuaki640@gmail.com>
2019-12-27 13:52:39 +0900

    フォームをいじる
    - placeholderを削除
    - formにaction属性を追加
    - 送信ボタンを作る

--- resources/views/user/user_post.blade.php
+++ resources/views/user/user_post.blade.php
@@ -3,22 +3,27 @@
 @section('title','月次報告登録')
 
 @section('content')
-    <form>
+
+
+
+    <form action="{{url("/user_confirm")}}" method="post">
+        @csrf
         <div class="form-group">
             <label for="name">報告者名</label>
-            <input type="text" class="form-control" id="name" placeholder="報告者名">
+            <input type="text" class="form-control" id="name">
         </div>
         <div class="form-group">
             <label for="numOfKumite">組手手かざし人数</label>
-            <input type="number" class="form-control" id="numOfKumite" placeholder="組手手かざし人数">
+            <input type="number" class="form-control" id="numOfKumite">
         </div>
         <div class="form-group">
             <label for="numOfNonKumite">未組手手かざし人数</label>
-            <input type="number" class="form-control" id="numOfNonKumite" placeholder="未組手手かざし人数">
+            <input type="number" class="form-control" id="numOfNonKumite">
         </div>
         <div class="form-group">
             <label for="numOfVisitors">未組手来場者数</label>
-            <input type="number" class="form-control" id="numOfVisitors" placeholder="未組手来場者数">
+            <input type="number" class="form-control" id="numOfVisitors">
         </div>
+        <button type="submit" class="btn btn-primary" value="post">登録</button>
     </form>
 @endsection
