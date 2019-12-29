da290d3c02bc2ef08eabf9044000b380bec95dfe
yasuaki640 <yasuaki640@gmail.com>
2019-12-29 20:57:39 +0900

    確認画面のルーティングを追加

--- routes/web.php
+++ routes/web.php
@@ -15,4 +15,6 @@ use \Illuminate\Support\Facades\Route;
 
 Route::get('/admin_index', 'CrudMonthlyReportController@index');
 
-Route::get('/user_post','CrudMonthlyReportController@post');
+Route::get('/user_post', 'CrudMonthlyReportController@post');
+
+Route::get('/user_confirm', 'CrudMonthlyReportController@confirm');
