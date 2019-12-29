726fb37fbd6d19822c67a838d2f2b9f10de6fb86
yasuaki640 <yasuaki640@gmail.com>
2019-12-29 20:51:22 +0900

    確認画面のモックを作成

---
+++ resources/views/user/user_confirm.blade.php
@@ -0,0 +1,22 @@
+@extends('layouts.common_layout')
+
+@section('title','入力確認')
+
+@section('content')
+    <div class="row">
+        <div class="col">報告者名</div>
+        <div class="col">yasu</div>
+    </div>
+    <div class="row">
+        <div class="col">組手手かざし人数</div>
+        <div class="col">yasu</div>
+    </div>
+    <div class="row">
+        <div class="col">未組手手かざし人数</div>
+        <div class="col">yasu</div>
+    </div>
+    <div class="row">
+        <div class="col">未組手来場者数</div>
+        <div class="col">yasu</div>
+    </div>
+@endsection
