d87dd10fe13785f893bea3ebbe8d447d370a4f2a
yasuaki640 <yasuaki640@gmail.com>
2019-12-31 21:33:59 +0900

    バリデーションエラーメッセージを表示させる

--- resources/views/user/user_post.blade.php
+++ resources/views/user/user_post.blade.php
@@ -6,23 +6,29 @@
 
     <h3>{{date("m")-1}} 月分の報告を入力してください</h3>
 
+    @if($errors->any())
+        @foreach($errors ->all()as$error)
+            <ul class="text-danger">{{$error}}</ul>
+        @endforeach
+    @endif
+
     <form action="{{action('CrudMonthlyReportController@confirm')}}" method="post">
         @csrf
         <div class="form-group">
             <label for="name">報告者名</label>
-            <input type="text" class="form-control" id="name">
+            <input type="text" class="form-control" id="name" value="{{old('name')}}">
         </div>
         <div class="form-group">
             <label for="numOfKumite">組手手かざし人数</label>
-            <input type="number" class="form-control" id="numOfKumite">
+            <input type="number" class="form-control" id="numOfKumite" value="{{old('numOfKumite')}}">
         </div>
         <div class="form-group">
             <label for="numOfNonKumite">未組手手かざし人数</label>
-            <input type="number" class="form-control" id="numOfNonKumite">
+            <input type="number" class="form-control" id="numOfNonKumite" value="{{old('numOfNonKumite')}}">
         </div>
         <div class="form-group">
             <label for="numOfVisitors">未組手来場者数</label>
-            <input type="number" class="form-control" id="numOfVisitors">
+            <input type="number" class="form-control" id="numOfVisitors" value="{{old('numOfVisitors')}}">
         </div>
         <button type="submit" class="btn btn-primary" value="post">登録</button>
     </form>
