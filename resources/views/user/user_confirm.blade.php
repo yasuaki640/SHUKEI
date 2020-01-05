0f639b7ebef2f0c06b620b41732af2d9945eaa8a
yasuaki640 <yasuaki640@gmail.com>
2020-01-05 14:19:12 +0900

    確認画面に入力内容を表示

--- resources/views/user/user_confirm.blade.php
+++ resources/views/user/user_confirm.blade.php
@@ -3,20 +3,21 @@
 @section('title','入力確認')
 
 @section('content')
+    @csrf
     <div class="row">
         <div class="col">報告者名</div>
-        <div class="col">yasu</div>
+        <div class="col">{{$input->name}}</div>
     </div>
     <div class="row">
         <div class="col">組手手かざし人数</div>
-        <div class="col">yasu</div>
+        <div class="col">{{$input->numOfKumite}}</div>
     </div>
     <div class="row">
         <div class="col">未組手手かざし人数</div>
-        <div class="col">yasu</div>
+        <div class="col">{{$input->numOfNonKumite}}</div>
     </div>
     <div class="row">
         <div class="col">未組手来場者数</div>
-        <div class="col">yasu</div>
+        <div class="col">{{$input->numOfVisitors}}</div>
     </div>
 @endsection
