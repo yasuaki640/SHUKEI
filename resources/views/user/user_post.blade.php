feaf29f29579064ffc728ecfd829296677a79c13
yasuaki640 <yasuaki640@gmail.com>
2020-01-05 14:18:49 +0900

    name属性の追加、バリデーションエラーの場合には入力した内容を保持

--- resources/views/user/user_post.blade.php
+++ resources/views/user/user_post.blade.php
@@ -4,31 +4,33 @@
 
 @section('content')
 
-    <h3>{{date("m")-1}} 月分の報告を入力してください</h3>
+    <h3>{{date("m")}} 月分の報告を入力してください</h3>
 
     @if($errors->any())
-        @foreach($errors ->all()as$error)
+        @foreach($errors->all() as $error)
             <ul class="text-danger">{{$error}}</ul>
         @endforeach
     @endif
-
-    <form action="{{action('CrudMonthlyReportController@confirm')}}" method="post">
+    <form action="{{action("CrudMonthlyReportController@confirm")}}" method="post">
         @csrf
         <div class="form-group">
             <label for="name">報告者名</label>
-            <input type="text" class="form-control" id="name" value="{{old('name')}}">
+            <input type="text" class="form-control" id="name" name="name" value="{{old('name')}}">
         </div>
         <div class="form-group">
             <label for="numOfKumite">組手手かざし人数</label>
-            <input type="number" class="form-control" id="numOfKumite" value="{{old('numOfKumite')}}">
+            <input type="number" class="form-control" id="numOfKumite" name="numOfKumite"
+                   value="{{old('numOfKumite')}}">
         </div>
         <div class="form-group">
             <label for="numOfNonKumite">未組手手かざし人数</label>
-            <input type="number" class="form-control" id="numOfNonKumite" value="{{old('numOfNonKumite')}}">
+            <input type="number" class="form-control" id="numOfNonKumite" name="numOfNonKumite"
+                   value="{{old('numOfNonKumite')}}">
         </div>
         <div class="form-group">
             <label for="numOfVisitors">未組手来場者数</label>
-            <input type="number" class="form-control" id="numOfVisitors" value="{{old('numOfVisitors')}}">
+            <input type="number" class="form-control" id="numOfVisitors" name="numOfVisitors"
+                   value="{{old('numOfVisitors')}}">
         </div>
         <button type="submit" class="btn btn-primary" value="post">登録</button>
     </form>
