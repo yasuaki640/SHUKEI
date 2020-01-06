b13d9227a055417336216e926673616c14caa92a
yasuaki640 <yasuaki640@gmail.com>
2020-01-06 16:09:18 +0900

    bootstrapのテーブルクラスを適用

--- resources/views/user/user_confirm.blade.php
+++ resources/views/user/user_confirm.blade.php
@@ -4,20 +4,24 @@
 
 @section('content')
     @csrf
-    <div class="row">
-        <div class="col">報告者名</div>
-        <div class="col">{{$input->name}}</div>
-    </div>
-    <div class="row">
-        <div class="col">組手手かざし人数</div>
-        <div class="col">{{$input->numOfKumite}}</div>
-    </div>
-    <div class="row">
-        <div class="col">未組手手かざし人数</div>
-        <div class="col">{{$input->numOfNonKumite}}</div>
-    </div>
-    <div class="row">
-        <div class="col">未組手来場者数</div>
-        <div class="col">{{$input->numOfVisitors}}</div>
-    </div>
+    <table class="table">
+        <tbody>
+        <tr>
+            <th scope="row">報告者名</th>
+            <td>{{$input->name}}</td>
+        </tr>
+        <tr>
+            <th scope="row">組手手かざし人数</th>
+            <td>{{$input->numOfKumite}}</td>
+        </tr>
+        <tr>
+            <th scope="row">未組手手かざし人数</th>
+            <td>{{$input->numOfNonKumite}}</td>
+        </tr>
+        <tr>
+            <th scope="row">未組手来場者数</th>
+            <td>{{$input->numOfVisitors}}</td>
+        </tr>
+        </tbody>
+    </table>
 @endsection
