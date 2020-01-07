ae97c89461aa607f6cc8b62ecf8e1e5528fe3fbc
yasuaki640 <yasuaki640@gmail.com>
2020-01-07 22:19:07 +0900

    viewに各カラムを持たせる

--- resources/views/user/user_confirm.blade.php
+++ resources/views/user/user_confirm.blade.php
@@ -3,27 +3,35 @@
 @section('title','入力確認')
 
 @section('content')
-    @csrf
-
     <h5 class="text-center my-4">以下の内容で登録します。</h5>
-    <table class="table">
-        <tbody>
-        <tr>
-            <th scope="row">報告者名</th>
-            <td>{{$input->name}}</td>
-        </tr>
-        <tr>
-            <th scope="row">組手手かざし人数</th>
-            <td>{{$input->numOfKumite}}</td>
-        </tr>
-        <tr>
-            <th scope="row">未組手手かざし人数</th>
-            <td>{{$input->numOfNonKumite}}</td>
-        </tr>
-        <tr>
-            <th scope="row">未組手来場者数</th>
-            <td>{{$input->numOfVisitors}}</td>
-        </tr>
-        </tbody>
-    </table>
+    <form action="{{action('CrudMonthlyReportController@create')}}" method="post">
+        @csrf
+        <table class="table">
+            <tbody>
+            <tr>
+                <th scope="row">報告者名</th>
+                <td>{{$input->name}}</td>
+            </tr>
+            <tr>
+                <th scope="row">組手手かざし人数</th>
+                <td>{{$input->numOfKumite}}</td>
+            </tr>
+            <tr>
+                <th scope="row">未組手手かざし人数</th>
+                <td>{{$input->numOfNonKumite}}</td>
+            </tr>
+            <tr>
+                <th scope="row">未組手来場者数</th>
+                <td>{{$input->numOfVisitors}}</td>
+            </tr>
+            </tbody>
+        </table>
+        <input type="hidden" name="name" value="{{$input->name}}">
+        <input type="hidden" name="numOfKumite" value="{{$input->numOfKumite}}">
+        <input type="hidden" name="numOfNonKumite" value="{{$input->numOfNonKumite}}">
+        <input type="hidden" name="numOfVisitors" value="{{$input->numOfVisitors}}">
+        <button type="submit" class="btn btn-primary" value="post">登録</button>
+    </form>
+
+
 @endsection
