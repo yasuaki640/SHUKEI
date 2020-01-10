c31f01b119a10cf9c1fab7bc70cacae597bb3c2d
yasuaki640 <yasuaki640@gmail.com>
2020-01-10 20:02:11 +0900

    id属性は不要なので削除

--- resources/views/user/user_post.blade.php
+++ resources/views/user/user_post.blade.php
@@ -15,21 +15,21 @@
         @csrf
         <div class="form-group">
             <label for="name">報告者名</label>
-            <input type="text" class="form-control" id="name" name="name" value="{{old('name')}}">
+            <input type="text" class="form-control" name="name" value="{{old('name')}}">
         </div>
         <div class="form-group">
             <label for="numOfKumite">組手手かざし人数</label>
-            <input type="number" class="form-control" id="numOfKumite" name="numOfKumite"
+            <input type="number" class="form-control" name="numOfKumite"
                    value="{{old('numOfKumite')}}">
         </div>
         <div class="form-group">
             <label for="numOfNonKumite">未組手手かざし人数</label>
-            <input type="number" class="form-control" id="numOfNonKumite" name="numOfNonKumite"
+            <input type="number" class="form-control" name="numOfNonKumite"
                    value="{{old('numOfNonKumite')}}">
         </div>
         <div class="form-group">
             <label for="numOfVisitors">未組手来場者数</label>
-            <input type="number" class="form-control" id="numOfVisitors" name="numOfVisitors"
+            <input type="number" class="form-control" name="numOfVisitors"
                    value="{{old('numOfVisitors')}}">
         </div>
         <button type="submit" class="btn btn-primary" value="post">確認</button>
